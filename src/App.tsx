99625f38bdb74a7c39dd663682025eb5d58db8a8
Daniel Alvarado Perez <97266996+danieleconomist99@users.noreply.github.com>
2025-11-24 23:14:18 -0500

    Replace HashRouter with BrowserRouter in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -16,18 +13,13 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-
-      <Router>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-
-          {/* Ruta de inmersión TEMPORAL usando Index */}
-          <Route path="/inmersion" element={<Index />} />
-
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </Router>
-
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
